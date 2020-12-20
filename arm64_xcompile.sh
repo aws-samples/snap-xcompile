0172a22e0b1756c50ae73488375b2184aaa993e7
Adi Singh <adsnghw@amazon.com>
2020-12-20 23:02:23 +0100

    Added sample source code

--- arm64_xcompile.sh
+++ arm64_xcompile.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+function get_status {
+	echo aws ec2 describe-tags \
+		--filters Name=resource-id,Values=$1 \
+				  Name=key,Values=Status \
+		--query "Tags[0].Value" --output text
+}  
+
 # Create s3 bucket
 uuid=$(head -c 16 /proc/sys/kernel/random/uuid)
 #echo $uuid
 name=arm64-snap-$uuid
 echo "- Creating S3 bucket"
-#aws s3 mb s3://$bucket
+aws s3 mb s3://$bucket
 
 # Upload code files to bucket
-#aws s3 cp src/ s3://$bucket/src --recursive
-#aws s3 cp snap/ s3://$bucket/snap --recursive
+echo "- Uploading source code to bucket"
+aws s3 cp src/ s3://$bucket/src --recursive
+aws s3 cp snap/ s3://$bucket/snap --recursive
 
 # initiate cfn stack
 echo "- Setting up xcompile resources"
@@ -24,60 +32,58 @@ echo -e "\t- Stack Name: $name"
 echo -e "\t- Stack ARN: $stack_arn"
 
 # while loop checking for stack outputs -> ec2 ID
-echo "Spinning up EC2 instance"
+echo -e "- Spinning up EC2 instance\c"
 ec2_id='None'
 
 while [ $ec2_id == 'None' ]; do
 	sleep 1
+	echo -e '.\c'
 	ec2_id=$(aws cloudformation describe-stacks \
 		--stack-name $stack_arn \
 		--query "Stacks[0].Outputs[?OutputKey=='InstanceId'].OutputValue" \
 		--output text)
-	echo -e '.\c'
 done
 
-echo -e "\nInstance ID: $ec2_id"
-echo "Installing AWS tools"
+echo -e "\n\t- Instance ID: $ec2_id"
+echo -e "- Installing AWS tools\c"
 
 # while loop fetching and printing user data output
 # complete when user data finished
 status=''
 
 while [ -z $status ]; do
-	echo -e '.\c'
 	sleep 1
-
-	status=$(aws ec2 describe-tags \
-		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
-		--query "Tags[0].Value" --output text)
+	echo -e '.\c'
+	status=$(get_status $ec2_id)
+#	status=$(aws ec2 describe-tags \
+#		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
+#		--query "Tags[0].Value" --output text)
 done
 
-status=$(aws ec2 describe-tags \
-		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
-		--query "Tags[0].Value" --output text)
-
-echo "Configuring machine"
+echo -e "\n- Configuring machine\c"
 while [ $status == "CONFIGURING" ]; do
 	echo -e '.\c'
 	sleep 1
 
-	status=$(aws ec2 describe-tags \
-		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
-		--query "Tags[0].Value" --output text)
+	status=$(get_status $ec2_id)
+#	status=$(aws ec2 describe-tags \
+#		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
+#		--query "Tags[0].Value" --output text)
 done
 
-echo "Snapping"
+echo -e "\n- Snapping\c"
 while [ $status == "SNAPPING" ]; do
 	echo -e '.\c'
 	sleep 1
 
-	status=$(aws ec2 describe-tags \
-		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
-		--query "Tags[0].Value" --output text)
+	status=$(get_status $ec2_id)
+#	status=$(aws ec2 describe-tags \
+#		--filters Name=resource-id,Values=$ec2_id Name=key,Values=Status \
+#		--query "Tags[0].Value" --output text)
 done
 
 
-echo 'complete!'
+echo -e '\ncomplete!'
 
 # download snap from s3
 
