ec7448059417f9ad71474f9926b28ab6d7b478cb
Adi Singh <adsnghw@amazon.com>
2020-12-20 17:40:04 +0100

    Fixed bucket name error

--- arm64_xcompile.sh
+++ arm64_xcompile.sh
@@ -3,16 +3,16 @@
 # Create s3 bucket
 uuid=$(head -c 16 /proc/sys/kernel/random/uuid)
 #echo $uuid
-bucket-name="arm64-snap-$uuid"
+bucket="arm64-snap-$uuid"
 echo "Creating S3 bucket..."
-aws s3 mb s3://$bucket-name
+aws s3 mb s3://$bucket
 
 # Upload code files to bucket
-aws s3 cp src/ s3://$bucket-name/src --recursive
-aws s3 cp snap/ s3://$bucket-name/snap --recursive
+aws s3 cp src/ s3://$bucket/src --recursive
+aws s3 cp snap/ s3://$bucket/snap --recursive
 
 # Replace bucket name in cfn stack 
-sed -i '' 's/S3_BUCKET/$bucket-name/g' arm64_cfn.yaml
+sed -i '' 's/S3_BUCKET/$bucket/g' arm64_cfn.yaml
 #sed -e "/USER_DATA/{r arm64_snapcraft.sh" -e "d}" arm64_cfn.yaml
 
 
