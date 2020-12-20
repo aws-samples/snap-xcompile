58fea5db185af0311d97aba152bdb1c2b35b6a90
Adi Singh <adsnghw@amazon.com>
2020-12-20 21:38:29 +0100

    Tag retrieval in script

--- arm64_snapcraft.sh
+++ arm64_snapcraft.sh
@@ -11,9 +11,9 @@ mkdir -p /tmp/robomaker_snap
 cd tmp/robomaker_snap
 aws s3 cp s3://S3_BUCKET/src src/ --recursive
 aws s3 cp s3://S3_BUCKET/snap snap/ --recursive
+
 snapcraft --use-lxd
 ls .
-
 aws s3 cp hello-world_0.1_arm64.snap s3://S3_BUCKET/
 
 ..just create temp bucket..download snap at end... delete bucket...
