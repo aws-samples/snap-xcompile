e197e927de82b136f1ec79d37e47448e388f60c7
Adi Singh <adsnghw@amazon.com>
2020-12-20 18:45:13 +0100

    Added query params

--- arm64_xcompile.sh
+++ arm64_xcompile.sh
@@ -5,19 +5,21 @@ uuid=$(head -c 16 /proc/sys/kernel/random/uuid)
 #echo $uuid
 bucket=arm64-snap-$uuid
 echo "Creating S3 bucket..."
-aws s3 mb s3://$bucket
+#aws s3 mb s3://$bucket
 
 # Upload code files to bucket
-aws s3 cp src/ s3://$bucket/src --recursive
-aws s3 cp snap/ s3://$bucket/snap --recursive
+#aws s3 cp src/ s3://$bucket/src --recursive
+#aws s3 cp snap/ s3://$bucket/snap --recursive
 
 # initiate cfn stack
 aws cloudformation create-stack \
 	--stack-name myteststack \
 	--template-body file://arm64_cfn.yaml \
 	--parameters ParameterKey=S3BucketName,ParameterValue=$bucket \
-	--capabilities CAPABILITY_IAM
+	--capabilities CAPABILITY_IAM \
+	--query 'StackId' --output text
 
+# while loop checking for stack outputs -> ec2 ID
 
 # while loop fetching and printing user data output
 # complete when user data finished
@@ -27,3 +29,5 @@ aws cloudformation create-stack \
 # delete cfn stack
 
 # delete s3 bucket
+
+#--query "Stacks[0].Outputs[?OutputKey=='DbUrl'].OutputValue" --output text
