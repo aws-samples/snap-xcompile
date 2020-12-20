2b01e39a459c08f1b0ad777325dc629168bbc1bd
Adi Singh <adsnghw@amazon.com>
2020-12-20 18:21:20 +0100

    Added cfn outputs

--- arm64_xcompile.sh
+++ arm64_xcompile.sh
@@ -11,11 +11,6 @@ aws s3 mb s3://$bucket
 aws s3 cp src/ s3://$bucket/src --recursive
 aws s3 cp snap/ s3://$bucket/snap --recursive
 
-# Replace bucket name in cfn stack 
-#sed -e "s/S3_BUCKET/${bucket}/g" arm64_cfn.yaml > arm64_cfn_$uuid.yaml
-#sed -e "/USER_DATA/{r arm64_snapcraft.sh" -e "d}" arm64_cfn.yaml
-
-
 # initiate cfn stack
 aws cloudformation create-stack \
 	--stack-name myteststack \
@@ -23,7 +18,6 @@ aws cloudformation create-stack \
 	--parameters ParameterKey=S3BucketName,ParameterValue=$bucket \
 	--capabilities CAPABILITY_IAM
 
-# delete cfn tmp file
 
 # while loop fetching and printing user data output
 # complete when user data finished
